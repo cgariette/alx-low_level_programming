e22f2569ba2059d75a496f0ef238dc20d944ed33
cgariette <clifftonafande@gmail.com>
2023-02-17 05:25:27 -0800

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
+if (x == 9 && y == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
