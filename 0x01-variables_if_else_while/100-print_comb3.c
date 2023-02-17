abe0d63f55018123753779855047329c9b237b64
cgariette <clifftonafande@gmail.com>
2023-02-17 05:52:55 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,13 @@ for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
+if (x != y)
+{
 putchar((x % 10) + '0');
 putchar((y % 10) + '0');
-if (x == 9 && y == 9)
-continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
