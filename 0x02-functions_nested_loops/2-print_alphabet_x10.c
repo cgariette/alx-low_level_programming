87ca435eadbe534d22295c0e25712965a776a8a1
cgariette <clifftonafande@gmail.com>
2023-02-21 04:47:41 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet_x10 -prints the alphabet in lowercase, followed by a new line.
+*/
+void print_alphabet_x10(void)
+{
+char alph;
+int i;
+for (i = 1; i <= 10; i++)
+{
+for (alph = 'a'; alph <= 'z'; alph++)
+_putchar(alph);
+}
+_putchar('\n');
+}
+
