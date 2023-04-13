cfeb019f16d833bbb0d67a2148840864f37b0d60
cgariette <clifftonafande@gmail.com>
2023-04-13 09:26:44 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,4 +21,14 @@ int create_file(const char *filename, char *text_content)
 		{
 			length++;
 		}
+	}
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, length);
 
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
