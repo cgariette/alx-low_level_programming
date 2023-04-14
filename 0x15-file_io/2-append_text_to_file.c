23127ad5ebdf2a009093ce4cc8df677f5edb3523
cgariette <clifftonafande@gmail.com>
2023-04-14 02:59:44 -0700

    indentation fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,18 +13,15 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int op, wr, length = 0;
-
 if (filename == NULL)
-		{
-			return (-1);
-		}
-
+{
+return (-1);
+}
 if (text_content != NULL)
 {
-	for (length = 0; text_content[length];)
-		length++;
+for (length = 0; text_content[length];)
+length++;
 }
-
 op = open(filename, O_WRONLY | O_APPEND);
 wr = write(op, text_content, length);
 
