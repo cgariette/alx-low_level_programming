e26a345f4a07fb2f3d00dc15d862604f19c88cff
cgariette <clifftonafande@gmail.com>
2023-11-07 04:07:21 -0800

    0 dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+struct dog
+{
+	*char name;
+	float age;
+	*char owner;
+};
